72e564ad2d6487f27a194cfcb3bb96f53c599f79
ashokvarma1188 <ashokvarma527@gmail.com>
2026-06-12 14:05:05 +0530

    backend

---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { registerUser };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -8,11 +9,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("SmartNest Backend Running");
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
