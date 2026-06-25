385e2a65867592a34aa96873cddc81c61b15acb2
Ashok Varma <ashokvarma5247@gmail.com>
2026-06-25 23:16:16 +0530

    Update testRoutes.js

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const protect = require("../middleware/authMiddleware");
 
 router.get("/profile", protect, (req, res) => {
